623c065c28193354ebada90f9f8a9f0078a61a1c
韩宇潇_16340069 <1078518902@qq.com>
2019-04-14 01:02:11 +0800

    数据库连接和数据库操作

---
+++ modules/teamModel.js
@@ -0,0 +1,35 @@
+const db = require('../config/db');
+const Sequelize = db.sequelize;
+const Team = Sequelize.import('../table/team');
+
+Team.sync({force: false});
+
+class TeamModel {
+    /**
+     * 创建 team 模型
+     * @param data
+     * @returns {Promise<*>}
+     */
+    static async createTeam(data) {
+        return await Team.create({
+            team_name: data.team_name,
+            logo: data.logo,
+            description: data.description,
+            tag:data.tag,
+        })
+    }
+
+    /**
+     * 查询取 team 详情数据
+     * @returns {Promise<Model>}
+     * @param team_id
+     */
+    static async getTeamDetail(team_id) {
+        return await Team.findOne({
+            where: {
+                team_id
+            }
+        })
+    }
+}
+module.exports = TeamModel;
