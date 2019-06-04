c7009fdef6327b04280391c8ae156a481a06b580
韩宇潇_16340069 <1078518902@qq.com>
2019-06-04 12:33:03 +0800

    toast API

--- routes/toast_api.js
+++ routes/toast_api.js
@@ -5,14 +5,14 @@ const CookieController = require('../controller/CookieController');
 router.prefix('/api/v1/toast');
 
 router.get('/', async (ctx) => {
-    let query_params = ctx.query;
     let result = null;
-    if (query_params.username) {
-        result = await ToastController.getToastByUsername(query_params.username)
+    let cookie_user = await CookieController.getUsernameFromCtx(ctx);
+    if (cookie_user !== -2) {
+        result = await ToastController.getToastByUsername(cookie_user)
     } else {
         result = {
-            code: 400,
-            msg: 'Wrong query param.',
+            code: 220,
+            msg: 'cookie超时，请重新登录',
             data: null
         }
     }
@@ -21,41 +21,9 @@ router.get('/', async (ctx) => {
 
 router.del('/Id/', async (ctx) => {
     let result = null;
-    // let cookie_user = await CookieController.getUsernameFromCtx(ctx);
-    // if (cookie_user !== ctx.request.body.leader) {
-    //     result = {
-    //         code: 220,
-    //         msg: 'cookie超时，请重新登录',
-    //         data: null
-    //     }
-    // } else {
-        let query_params = ctx.query;
-        if (query_params.id) {
-            result = await ToastController.deleteToastById(query_params.id)
-        } else {
-            result = {
-                code: 400,
-                msg: 'Wrong query param.',
-                data: null
-            }
-        }
-    // }
-    response(ctx, result)
-});
-
-router.del('/Username/', async (ctx) => {
-    let result = null;
-    // let cookie_user = await CookieController.getUsernameFromCtx(ctx);
-    // if (cookie_user !== ctx.request.body.leader) {
-    //     result = {
-    //         code: 220,
-    //         msg: 'cookie超时，请重新登录',
-    //         data: null
-    //     }
-    // } else {
     let query_params = ctx.query;
-    if (query_params.username) {
-        result = await ToastController.deleteToastByUsername(query_params.username)
+    if (query_params.id) {
+        result = await ToastController.deleteToastById(query_params.id)
     } else {
         result = {
             code: 400,
@@ -63,7 +31,21 @@ router.del('/Username/', async (ctx) => {
             data: null
         }
     }
-    // }
+    response(ctx, result)
+});
+
+router.del('/Username/', async (ctx) => {
+    let result = null;
+    let cookie_user = await CookieController.getUsernameFromCtx(ctx);
+    if (cookie_user !== -2) {
+        result = await ToastController.deleteToastByUsername(cookie_user)
+    } else {
+        result = {
+            code: 220,
+            msg: 'cookie超时，请重新登录',
+            data: null
+        }
+    }
     response(ctx, result)
 });
 
