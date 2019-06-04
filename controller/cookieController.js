9529ba39b52b2bfc0ceaf1d70851f7a63fb3fb61
Huang-Junjie <1582695658@qq.com>
2019-06-04 10:46:25 +0800

    update judgeCookie and getUsernameFromCtx

--- controller/cookieController.js
+++ controller/cookieController.js
@@ -4,45 +4,25 @@ global.redis = new Store();
 
 class CookieController {
     /**
-     * 判断前端请求是否携带了cookies
-     * 若携带了还会判断session是否过期
+     * 判断前端请求是否携带了正确的cookies
      * @param ctx
      * @returns
-     *  -1: 请求未携带cookies
-     *  -2：携带了cookies但是cookies已过期
-     *   0：cookies认证成功
+     *  -1: 请求未携带cookies或cookie失效
+     *  -0：cookies认证成功
      */
     static async judgeCookies(ctx) {
-        const SESSIONID = ctx.cookies.get('SESSIONID');
-        //没有携带cookies
-        if (!SESSIONID) {
-            return -1
-        }
-        // 如果有SESSIONID，就去redis里拿数据
-        const redisData = await redis.get(SESSIONID);
-
-        //携带了cookies但session已过期
-        if (!redisData) {
-            return -2
-        }
-
-        return 0
+        return ctx.session.username ? 0 : -1;
     }
 
     /**
      * 从前端的一个请求中通过cookies获得用户名
      * @param ctx
      * @returns
-     *  -1：未携带cookies
-     *  -2：携带的cookies无效或已过期
+     *  -1：cookies无效
      *  username: 用户名，一个字符串
      */
     static async getUsernameFromCtx(ctx) {
-        const username = ctx.session.username;
-        if (username == null) {
-            return -2
-        }
-        return username;
+        return ctx.session.username ? ctx.session.username : -2;
     }
 }
 
--- controller/userController.js
+++ controller/userController.js
@@ -12,52 +12,25 @@ require('./CookieController');
 class UserController {
 
     /**
-     * 判断前端请求是否携带了cookies
-     * 若携带了还会判断session是否过期
+     * 判断前端请求是否携带了正确的cookies
      * @param ctx
      * @returns
-     *  -1: 请求未携带cookies
-     *  -2：携带了cookies但是cookies已过期
-     *   0：cookies认证成功
+     *  -1: 请求未携带cookies或cookie失效
+     *  -0：cookies认证成功
      */
     static async judgeCookies(ctx) {
-        const SESSIONID = ctx.cookies.get('SESSIONID');
-        //没有携带cookies
-        if (!SESSIONID) {
-            return -1
-        }
-        // 如果有SESSIONID，就去redis里拿数据
-        const redisData = await redis.get(SESSIONID);
-    
-        //携带了cookies但session已过期
-        if (!redisData) {
-            return -2
-        }
-    
-        return 0
+        return ctx.session.username ? 0 : -1;
     }
 
     /**
      * 从前端的一个请求中通过cookies获得用户名
-     * @param ctx 
+     * @param ctx
      * @returns
-     *  -1：未携带cookies
-     *  -2：携带的cookies无效或已过期
-     *  username: 用户名，一个字符串 
+     *  -1：cookies无效
+     *  username: 用户名，一个字符串
      */
     static async getUsernameFromCtx(ctx) {
-        const flag = await UserController.judgeCookies(ctx);
-        if (flag === -1) {
-            return -1
-        }
-        else if (flag === -2) {
-    
-            return -2
-        }
-        const SESSIONID = ctx.cookies.get('SESSIONID')
-        const redisData = await redis.get(SESSIONID)
-    
-        return redisData.username
+        return ctx.session.username ? ctx.session.username : -1;
     }
     
     /**
