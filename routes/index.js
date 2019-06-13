70435cff9d6d0b61b98956991c41f9f7cb7be9aa
gongzq5 <765194873@qq.com>
2019-06-13 14:31:54 +0800

    A change about scores

--- routes/index.js
+++ routes/index.js
@@ -13,59 +13,65 @@ const ToastModel = require('../modules/toastModel');
 const ToastInfo = require('../utils/toast_info');
 const Op = Sequelize.Op
 const UserModel = require('../modules/userModel');
+const normal = require('../dist/index')
 
-router.post('/test', async (ctx, next) => {
-    let post_body = ctx.request.body
-    let current_user = 'hyx'
-    let result = undefined
-    let post_data = {
-        title: post_body.title,
-        introduction: post_body.introduction,
-        money: post_body.money,
-        score: post_body.score,
-        max_accepter_number: post_body.max_accepter_number,
-        publisher: current_user, // only session
-        type: post_body.type,
-        starttime: post_body.starttime,
-        endtime: post_body.endtime,
-        content: post_body.content,
-        questionnaire_path: post_body.questionnaire_path
-    }
-    try {
-        result = await TaskModel.createTask(post_data, post_body.range)
-        let task_id = result.get('task_id')
-        console.log(task_id)        
-        let task_money = (await TaskModel.searchTaskById(task_id)).get('money');
-        console.log(task_money)
-        await UserModel.updateUserMoney(current_user, -task_money);
+// router.post('/test', async (ctx, next) => {
+//     let post_body = ctx.request.body
+//     let current_user = 'hyx'
+//     let result = undefined
+//     let post_data = {
+//         title: post_body.title,
+//         introduction: post_body.introduction,
+//         money: post_body.money,
+//         score: post_body.score,
+//         max_accepter_number: post_body.max_accepter_number,
+//         publisher: current_user, // only session
+//         type: post_body.type,
+//         starttime: post_body.starttime,
+//         endtime: post_body.endtime,
+//         content: post_body.content,
+//         questionnaire_path: post_body.questionnaire_path
+//     }
+//     try {
+//         result = await TaskModel.createTask(post_data, post_body.range)
+//         let task_id = result.get('task_id')
+//         console.log(task_id)        
+//         let task_money = (await TaskModel.searchTaskById(task_id)).get('money');
+//         console.log(task_money)
+//         await UserModel.updateUserMoney(current_user, -task_money);
         
-        result = {
-            code: 200,
-            msg: "Success",
-            data: result
-        }
+//         result = {
+//             code: 200,
+//             msg: "Success",
+//             data: result
+//         }
         
-    } catch (err) {
-        result = {
-            code: 500,
-            msg: "Failed",
-            data: err.message
-        }
-        console.log(err)
-    }
+//     } catch (err) {
+//         result = {
+//             code: 500,
+//             msg: "Failed",
+//             data: err.message
+//         }
+//         console.log(err)
+//     }
 
-    ctx.response.status = result.code
-    ctx.body = {
-        code: result.code,
-        msg: result.msg,
-        data: result.data
-    }
-})
+//     ctx.response.status = result.code
+//     ctx.body = {
+//         code: result.code,
+//         msg: result.msg,
+//         data: result.data
+//     }
+// })
 
 router.get('/', async (ctx, next) => {
-    await ctx.render('index', {
-        title: 'Hello Koa 2!'
-    })
+    let search = ctx.query
+    let a = undefined
+    if (search.username != undefined && search.task_id != undefined && search.score != undefined) {
+        a = await TRModel.comfirm_complement(search.username, search.task_id, search.score)
+    } else {
+        a = 'Not implement'
+    }
+    ctx.body = a
 });
 
 router.get('/string', async (ctx, next) => {
